c22e47f79cb08b458c5e05742cfec417bde2e845
Zofie <github@zofie.be>
2025-07-24 12:46:32 +0200

    Added link to service to request via mail

--- js/index.js
+++ js/index.js
@@ -70,3 +70,17 @@ fetch('clients.json')
     console.error("Fout bij laden van clients.json:", error);
   });
 
+
+  // Scroll to block
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        e.preventDefault();
+        target.scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        });
+      }
+    });
+  });
